10339f5d83f80092f07af20302a6bac8bbc1566c
Steve Ellis <email@steveell.is>
2013-04-27 15:05:54 -0400

    Add initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makerpath::Application.config.secret_key_base = 'f85034ebbc882ae80d2d59ff6fb2a0fd9751e1161e256621dbe1bf7dd52033885f4fca9923da97e2259424dba08b809ae96a124807fe4b122882a926ad694b1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Makerpath::Application.config.session_store :encrypted_cookie_store, key: '_makerpath_session'
